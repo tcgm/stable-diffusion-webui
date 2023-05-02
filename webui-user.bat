92a66a4b936e3c19572f6fa7bad461fc205be883
TCGM <thecrazygamemaster@gmail.com>
2023-05-02 04:13:09 -0700

    2023-5-2

--- webui-user.bat
+++ webui-user.bat
@@ -1,8 +1,10 @@
 @echo off
 
-set PYTHON=
+set PYTHON=py -3.10
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=--medvram --listen --allow-code --enable-insecure-extension-access
+set COMMANDLINE_ARGS=--medvram --lowram --listen --allow-code --enable-insecure-extension-access --opt-split-attention
+
+start firefox http://localhost:7860
 
 call webui.bat
